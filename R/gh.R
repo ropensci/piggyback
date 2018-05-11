78e17c70050bff7574ed456ca17e0de23b6ed6a5
Carl Boettiger <cboettig@gmail.com>
2018-05-11 14:05:40 -0700

    provide some package structure

---
+++ R/gh.R
@@ -0,0 +1,139 @@
+## FIXME: allow for download of individual file by name, not just all files
+#' download_data("cboettig/ghdata")
+#' @importFrom httr GET add_headers write_disk
+#' @importFrom gh gh
+#' @export
+pb_download <- function(repo, file = NULL, dest = ".", tag = "latest"){
+
+  r <- strsplit(repo, "/")[[1]]
+  x <- release_info(repo, tag)
+  id <- vapply(x$assets, `[[`, character(1), "id")
+  file <- file.path(dest, vapply(x$assets, `[[`, character(1), "name"))
+
+  for(i in seq_along(id)){
+    resp <- gh_download_asset(r[[1]], r[[2]], id=id[i], file=file[i])
+    httr::stop_for_status(resp)
+  }
+
+  invisible(resp)
+}
+
+## gh() fails on this, so we do with httr. See https://github.com/r-lib/gh/issues/57
+gh_download_asset <- function(owner, repo, id, file, overwrite=TRUE,
+                              .token = get_token()
+                              ){
+  resp <- httr::GET(paste0("https://api.github.com/repos/", owner,"/",
+                   repo, "/", "releases/assets/", id,
+                   "?access_token=", .token),
+            add_headers(Accept = "application/octet-stream"),
+            write_disk(file, overwrite = overwrite))
+  httr::stop_for_status(resp)
+  invisible(resp)
+}
+
+##################### Generic helpers ##################
+release_info <- function(repo, tag="latest"){
+  r <- strsplit(repo, "/")[[1]]
+  if(tag == "latest"){
+    gh("/repos/:owner/:repo/releases/latest",
+       owner = r[[1]], repo = r[[2]])
+  } else {
+    gh("/repos/:owner/:repo/releases/tags/:tag",
+       owner = r[[1]], repo = r[[2]], tag = tag)
+  }
+}
+get_token <- function(){
+  Sys.getenv("GITHUB_PAT", Sys.getenv("GITHUB_TOKEN"))
+}
+#####################################################
+
+
+
+#' Create a new release on GitHub repo
+#'
+#' @importFrom jsonlite toJSON
+#' @importFrom httr content GET POST stop_for_status
+gh_new_release <- function(repo,
+                           tag,
+                           commit = "master",
+                           name = tag,
+                           body = "Data release",
+                           draft = FALSE,
+                           prerelease = FALSE,
+                           .token = get_token()){
+  r <- strsplit(repo, "/")[[1]]
+
+  payload <- list(
+    tag_name = tag,
+    target_commitish = commit,
+    name = name,
+    body = body,
+    draft =  draft,
+    prerelease = prerelease)
+
+  ## gh fails to pass body correctly??
+  #gh("/repos/:owner/:repo/releases", owner = r[[1]], repo = r[[2]],
+  #   .method = "POST", body = toJSON(payload,auto_unbox = TRUE), encode="json")
+
+  token <- .token
+  resp <- httr::POST(paste0("https://api.github.com/repos/", r[[1]],"/",
+                     r[[2]], "/", "releases?access_token=", token),
+                     body = jsonlite::toJSON(payload,auto_unbox = TRUE))
+
+  httr::stop_for_status(resp)
+  release <- httr::content(resp)
+}
+
+
+
+#' Upload data to an existing release
+#'
+#' NOTE: you must first create a release if one does not already exists.
+#' @param repo Repository name in format "owner/repo".
+#' @param tag  tag for the release to which this data should be attached.
+#' @param file path to file to be uploaded
+#' @param name name for uploaded file. If not provided will use the basename of
+#' `file` (i.e. filename without directory)
+#' @param overwrite overwrite any existing file with the same name already
+#'  attached to the on release?
+#' @param .token GitHub authentication token
+#' @examples
+#' \dontrun{
+#' # Needs your real token to run
+#' Sys.setenv(GITHUB_TOKEN = "xxxxx")
+#'
+#' readr::write_tsv(mtcars,"mtcars.tsv.xz")
+#' ghdata_upload("cboettig/piggyback", "v0.0.3", "mtcars.tsv.xz")
+#' }
+#' @importFrom httr progress upload_file POST stop_for_status
+#' @export
+pb_upload <- function(repo,
+                      tag,
+                      file,
+                      name = NULL,
+                      overwrite = FALSE,
+                      .token = get_token()){
+
+  if(is.null(name)){
+    name <- basename(file)
+  }
+
+  x <- release_info(repo, tag)
+
+  if(overwrite){
+    ## Check if file exists
+
+    ## Delete file from release
+  }
+
+
+  r <- httr::POST(sub("\\{.+$", "", x$upload_url), query = list(name = name),
+                  body = httr::upload_file(file), httr::progress("up"),
+                  httr::authenticate(.token, "x-oauth-basic", "basic"))
+  cat("\n")
+  httr::stop_for_status(r)
+  invisible(r)
+}
+
+## consider overwrite option for upload?  (delete and re-upload asset)
+## consider plain delete function for upload
