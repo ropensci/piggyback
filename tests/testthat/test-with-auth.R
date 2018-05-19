f743a3eddfbea3d767e5ead230c2b66f631490cf
Carl Boettiger <cboettig@gmail.com>
2018-05-18 23:05:14 -0700

    push

--- tests/testthat/test-with-auth.R
+++ tests/testthat/test-with-auth.R
@@ -9,6 +9,26 @@ testthat::test_that("We can upload data",{
   data <- readr::write_tsv(datasets::iris, "iris.tsv.gz")
   pb_upload("cboettig/piggyback", file = "iris.tsv.gz", tag = "v0.0.1", overwrite = TRUE)
 })
+
+
+testthat::test_that("We can push and pull data",{
+  testthat::skip_if(piggyback:::get_token() == "")
+
+  cur <- getwd()
+  tmp <- tempdir()
+  proj_dir <- file.path(tmp, "piggyback-test")
+  suppressMessages(usethis::create_project(proj_dir,
+                                           open=FALSE))
+  setwd(proj_dir)
+
+  pb_track("*.tsv")
+  out <- pb_pull(.repo = "cboettig/piggyback")
+  testthat::expect_true(out)
+
+  pb_push(.repo = "cboettig/piggyback")
+  setwd(cur)
+
+  })
 ## Create and delete a new test repository?
 
 #repo <- "cboettig/piggyback-test"
