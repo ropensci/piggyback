3e23e9a28f3d60890a11f2fccd5b2e2fc43a17ab
Tan-DESKPC <tan@tanho.ca>
2022-04-25 19:17:11 -0400

    pb_download_url better handles multiple files, including missings

--- R/pb_download_url.R
+++ R/pb_download_url.R
@@ -19,11 +19,19 @@ pb_download_url <- function(file = NULL,
                             tag = "latest",
                             .token = gh::gh_token()) {
   df <- pb_info(repo, tag, .token)
-  if (is.null(file)) {
-    return(df$browser_download_url)
-  } else if (file %in% df$file_name) {
-    return(df[file == df$file_name, "browser_download_url"])
-  } else {
-    stop(paste("file", file, "not found in release", tag, "for repo", repo))
+
+  if(is.null(file)) return(df$browser_download_url)
+
+  if(any(!file %in% df$file_name)) {
+
+    missing <- file[!file %in% df$file_name]
+
+    cli::cli_warn("file {.val {missing}} not found in release {.val {tag}} for repo {.val {repo}}")
+
+    file <- file[file %in% df$file_name]
   }
+
+  if(length(file) == 0) return(cli::cli_abort("No download URLs to return."))
+
+  return(df[df$file_name %in% file,"browser_download_url"])
 }
