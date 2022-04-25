9da26eae330f9f64bd436b8f1c7e65d20db863b9
Tan-DESKPC <tan@tanho.ca>
2022-04-24 23:58:01 -0400

    update docs

--- R/pb_list.R
+++ R/pb_list.R
@@ -1,21 +1,16 @@
-
 #' List all assets attached to a release
 #' @inheritParams pb_download
-#' @param tag which release tag do we want information for? If `NULL` (default),
+#' @param tag which release tag(s) do we want information for? If `NULL` (default),
 #' will return a table for all available release tags.
-#' @return a data.frame of release asset names, (normalized to local paths), release tag,
-#' timestamp, owner, and repo.
-#' @details To preserve path information, local path delimiters are converted to `.2f`
-#' when files are uploaded as assets.  Listing will display the local filename,
-#' with asset names converting the `.2f` escape code back to the system delimiter.
+#' @return a data.frame of release asset names, release tag, timestamp, owner, and repo.
 #' @examples
 #' \dontrun{
 #' pb_list("cboettig/piggyback-tests")
 #' }
+#' @seealso `pb_releases` for a list of all releases in repository
 #' @export
 pb_list <- function(repo = guess_repo(),
                     tag = NULL,
-                    ignore = "manifest.json",
                     .token = get_token()) {
   df <- pb_info(repo, tag, .token)
   df[c("file_name", "size", "timestamp", "tag", "owner", "repo")]
