d0256afb80a0ec606e9ef667701ff2315137d8cf
Tan Ho <tan@tanho.ca>
2022-07-07 00:50:03 -0400

    minimize non-token calls

--- tests/testthat/test-pb_info.R
+++ tests/testthat/test-pb_info.R
@@ -16,22 +16,10 @@ testthat::with_mock(
       }
     )
 
-    test_that(
-      "we can list releases without auth", {
-        skip_if_offline("api.github.com")
-
-        x <- pb_releases(
-          repo = "cboettig/piggyback-tests"
-        )
-
-        expect_true(nrow(x) > 1)
-        expect_true("v0.0.1" %in% x$tag_name)
-      }
-    )
   })
 
-context("Get release/asset info with default token")
-## We also expect users to be able to access public repos with their own token
+context("Get release/asset info with default/CI token")
+
 test_that(
   "we can list files with default auth", {
     skip_if_offline("api.github.com")
