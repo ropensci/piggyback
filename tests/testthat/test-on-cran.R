965f37768186be6a0086582f34ee977282276172
Tan Ho <tan@tanho.ca>
2022-07-06 23:12:27 -0400

    tests for utils
    test utilities
    
    rm extraneous test on cran

--- tests/testthat/test-on-cran.R
+++
@@ -1,9 +0,0 @@
-context("on-cran")
-
-
-testthat::test_that(
-  "no guarentees of anything on CRAN, better check", {
-  expect_true(1+1 == 2)
-
-  }
-)
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,39 @@ test_that("compact works",{
     list("a","b")
   )
 })
+
+test_that("guess_repo works",{
+  skip_if_offline("api.github.com")
+  skip_if_not_installed("usethis")
+
+  temp_dir <- tempdir(check = TRUE)
+
+  usethis::create_from_github(
+    repo_spec = "cboettig/piggyback-tests",
+    destdir = temp_dir,
+    fork = FALSE,
+    rstudio = FALSE,
+    open = FALSE,
+    protocol = "https"
+  )
+
+  repo_path <- file.path(temp_dir,"piggyback-tests")
+  on.exit(unlink(repo_path, recursive = TRUE))
+
+  expect_equal(
+    guess_repo(repo_path),
+    "cboettig/piggyback-tests"
+  )
+
+})
+
+test_that("parse_repo works",{
+  expect_equal(
+    parse_repo("cboettig/piggyback-tests"),
+    c("cboettig","piggyback-tests")
+  )
+  expect_error(
+    parse_repo("cboettig-piggyback-tests"),
+    "Could not parse"
+  )
+})
