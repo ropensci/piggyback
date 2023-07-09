588b0c6957d9a7c6e9c8848f3aeee317729a72d5
Tan Ho <tan@tanho.ca>
2023-07-09 18:35:38 -0400

    add R option to disable caching

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,15 @@
 .onLoad <- function(libname,pkgname){
   #nocov start
-  cache_duration <- as.numeric(Sys.getenv("piggyback_cache_duration", unset = "600"))
-  assign(x = "pb_info",
-         value = memoise::memoise(pb_info, ~ memoise::timeout(cache_duration)),
-         envir = parent.env(environment()))
-  assign(x = "pb_releases",
-         value = memoise::memoise(pb_releases, ~ memoise::timeout(cache_duration)),
-         envir = parent.env(environment()))
+  use_cache <- getOption("piggyback.cache", default = TRUE)
+  if(!use_cache) cli::cli_alert_info("piggyback.cache set to FALSE - caching disabled")
+  if(use_cache){
+    cache_duration <- as.numeric(Sys.getenv("piggyback_cache_duration", unset = "600"))
+    assign(x = "pb_info",
+           value = memoise::memoise(pb_info, ~ memoise::timeout(cache_duration)),
+           envir = parent.env(environment()))
+    assign(x = "pb_releases",
+           value = memoise::memoise(pb_releases, ~ memoise::timeout(cache_duration)),
+           envir = parent.env(environment()))
+  }
   #nocov end
 }
