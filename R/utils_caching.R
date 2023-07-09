ec77b6fbf8be4a83c139d55d4d457fe8b81ad019
Tan Ho <tan@tanho.ca>
2023-07-09 18:42:38 -0400

    export cache clear

--- R/utils_caching.R
+++ R/utils_caching.R
@@ -5,8 +5,8 @@
 #' @examples .pb_cache_clear()
 #'
 #' @return invisible: TRUE on success
+#' @export
 #' @keywords internal
-
 .pb_cache_clear <- function(){
   # the ones memoised in zzz.R
   memoised_functions <- c("pb_info", "pb_releases")
