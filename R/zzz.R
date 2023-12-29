f970fd542261b97d4cf6b425c44a13ec1f490eda
Tan Ho <38083823+tanho63@users.noreply.github.com>
2023-12-29 13:25:02 -0500

    `pb_download` tries browser download URLs first to reduce ratelimiting (#110)
    Resolves #109
    
    ## Changes
    
    Adds `download_url` to `gh_download_asset` and then tries to download
    from there first, falling back to the previous API download and download
    redirection after that if for some reason it doesn't work.
    
    ## Other changes
    
    - Documentation for `pb_download` and `pb_upload` copyedited
    - Adds comments and some light refactoring to `pb_download()`
    - assertion to ensure either dest = length 1 or length of dest = length
    of files to download
      - minor readability/simplification improvements
    - Updates to test infrastructure
    - Now checks test token (TAN_GH_TOKEN) to see if it has push permissions
    to the test repositories (`tanho63/piggyback`,
    `tanho63/piggyback-tests`, `tanho63/piggyback-private`) which should
    reduce erroring on this step :crossed_fingers:

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,25 @@
+#nocov start
+
 .onLoad <- function(libname,pkgname){
-  #nocov start
+
+  # piggyback caches repository info only and does not cache download/upload commands
   use_cache <- getOption("piggyback.cache", default = TRUE)
+
   if(!use_cache) cli::cli_alert_info("piggyback.cache set to FALSE - caching disabled")
+
   if(use_cache){
+    # default 600 seconds = 10 minutes
     cache_duration <- as.numeric(Sys.getenv("piggyback_cache_duration", unset = "600"))
     assign(x = "pb_info",
            value = memoise::memoise(pb_info, ~ memoise::timeout(cache_duration)),
-           envir = parent.env(environment()))
+           envir = rlang::ns_env("piggyback"))
     assign(x = "pb_releases",
            value = memoise::memoise(pb_releases, ~ memoise::timeout(cache_duration)),
-           envir = parent.env(environment()))
+           envir = rlang::ns_env("piggyback"))
+    assign(x = ".check_test_token",
+           value = memoise::memoise(.check_test_token, ~ memoise::timeout(cache_duration)),
+           envir = rlang::ns_env("piggyback"))
   }
-  #nocov end
 }
+
+#nocov end
